056e194c8e1f767bed5432ad282437098b470297
Egie Hermawan <egiehermawan78@gmail.com>
2020-07-28 09:48:23 -0700

    pokemon-api

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+function Navbar() {
+    return (
+        <div className="Navbar">
+        Project Pokemon API Internship
+    </div>
+    )  
+}
+
+export default Navbar;
